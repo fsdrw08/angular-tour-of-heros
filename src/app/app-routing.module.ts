a754401dbdce0535e218c423c94dbde1acd3271c
Windom Wu <windomwu@MacdeMBP-4.wirelessdomain>
2019-09-10 15:28:47 +0800

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
-
-const routes: Routes = [];
+//define an array of routes with a single router to that component
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  { path: 'detail/:id', component: HeroDetailComponent },
+]
 
 @NgModule({
+  //initialize the router and start it listening for browser locaition changes
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
