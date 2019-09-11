11596b31b1f6137cc963474eb4960024c53adb17
Windom Wu <windomwu@MacdeMBP-4.wirelessdomain>
2019-09-11 22:58:58 +0800

    http

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,23 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
-import { HEROES } from './ mock-heroes';
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetch heros');
-    return of (HEROES);
+  private heroesUrl = 'api/heroes';  // URL to web api
+  
+  /**
+  * Handle Http operation that failed.
+  * Let the app continue.
+  * @param operation - name of the operation that failed
+  * @param result - optional value to return as the observable result
+  */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
-  getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: get Hero by ID ${id}`);
-    return of (HEROES.find(hero => hero.id === id));
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError<Hero[]>('getHeroes',[]))
+      );
   }
+/*
+  getHero(id: number): Observable<Hero> {
+    return this.http.get<Hero[]>()
+    //of (HEROES.find(hero => hero.id === id));
+  }
+*/  
+  private log(message: string) {
+    this.messageService.add(`Heroservices: ${message}`)
+  }
+
   
-  constructor(private messageService : MessageService) { }
+
+  constructor(
+    private http: HttpClient,
+    private messageService : MessageService) { }
 }
