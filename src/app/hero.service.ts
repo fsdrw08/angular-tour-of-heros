7e59134fe9d0d681fb22b65537ab6faf2df0f206
fsdrw08 <drw_08@hotmail.com>
2019-09-11 10:06:51 +0800

    If you paste localhost:4200/detail/11 in the browser address bar, the router navigates to the detail view for the hero with id: 11, 'Dr Nice'.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,5 +13,10 @@ export class HeroService {
     this.messageService.add('HeroService: fetch heros');
     return of (HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService: get Hero by ID ${id}`);
+    return of (HEROES.find(hero => hero.id === id));
+  }
   constructor(private messageService : MessageService) { }
 }
